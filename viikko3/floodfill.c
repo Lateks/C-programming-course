77f9f4dd68b6f2b86c4b0689287296b30d33f5af
Laura Leppänen <laura.leppanen@iki.fi>
2011-09-18 20:48:12 +0300

    Finished the last exercises of week 3.

--- viikko3/floodfill.c
+++ viikko3/floodfill.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
-struct map {
+#define BAD_CHAR 'X'
+
+typedef struct map {
     int width;
     int height;
     char* data;
-};
+} Map;
 
 /* Exercise 6 */
 bool is_line_break(char ch) {
@@ -37,8 +40,13 @@ char* load_data(char* filename) {
 
     int data_length = get_file_length(filename);
     char* data = malloc((data_length + 1)*sizeof(char));
+    if (!data)
+        return NULL;
+
     size_t chars_read = fread(data, sizeof(char), data_length, fptr);
     fclose(fptr);
+
+    data[data_length-1] = '\0';
 
     if (chars_read != (size_t) data_length) {
         free(data);
@@ -49,16 +57,26 @@ char* load_data(char* filename) {
 }
 
 /* Exercise 9 */
-struct map *load_map(char* filename) {
+Map* load_map(char* filename) {
     char* data = load_data(filename);
     if (!data)
         return NULL;
-    struct map *new_map = malloc(sizeof(struct map));
+    Map *new_map = malloc(sizeof(Map));
+    if (!new_map)
+        return NULL;
+
     new_map->data = data;
 
     int width = 0;
-    while (!is_line_break(data[width]))
-        width++;
+    do {
+        if (is_line_break(data[width])) {
+            width++;
+            bool is_windows = is_line_break(data[width+1]);
+            if (is_windows)
+                width++;
+            break;
+        }
+    } while (++width);
     new_map->width = width;
 
     unsigned int data_length = get_file_length(filename);
@@ -73,9 +91,66 @@ struct map *load_map(char* filename) {
 }
 
 /* Exercise 10 */
-void print_map(struct map* the_map) {
-    printf("width: %d, height: %d\n%s", the_map->width, the_map->height,
+void print_map(Map* the_map) {
+    printf("width: %d, height: %d\n%s\n", the_map->width, the_map->height,
         the_map->data);
+}
+
+/* Exercise 13 */
+bool valid_coordinates(Map *the_map, int x, int y) {
+    if ((x >= 0 && x < the_map->width) &&
+        (y >= 0 && y < the_map->height))
+        return true;
+    return false;
+}
+
+/* Exercise 11 */
+char get_char_at(Map *the_map, int x, int y) {
+    if (valid_coordinates(the_map, x, y)) {
+        unsigned int coord = y * the_map->width + x;
+        return the_map->data[coord];
+    }
+    return BAD_CHAR;
+}
+
+/* Exercise 12 */
+void set_char_at(Map *the_map, int x, int y, char val) {
+    if (!valid_coordinates(the_map, x, y))
+        return;
+    unsigned int coord = y * the_map->width + x;
+    the_map->data[coord] = val;
+}
+
+char random_digit_symbol() {
+    int random = rand()/((float) RAND_MAX)*9 + 1;
+    char c = '0' + random;
+    return c;
+}
+
+/* Exercise 14. Disregarded the unnecessary parameter wall
+in the assignment. */
+void flood_fill(Map *the_map) {
+    for (int y = 0; y < the_map->height; y++)
+        for (int x = 0; x < the_map->width; x++) {
+            if (get_char_at(the_map, x, y) != ' ')
+                continue;
+            char c = random_digit_symbol();
+            set_char_at(the_map, x, y, c);
+        }
+}
+
+/* Exercise 15. Disregarded the unnecessary parameter wall in
+the assignment. */
+void rec_flood_fill(Map *the_map, int x, int y) {
+    if (get_char_at(the_map, x, y) != ' ')
+        return;
+    char c = random_digit_symbol();
+    set_char_at(the_map, x, y, c);
+    print_map(the_map);
+    rec_flood_fill(the_map, x-1, y);
+    rec_flood_fill(the_map, x+1, y);
+    rec_flood_fill(the_map, x, y-1);
+    rec_flood_fill(the_map, x, y+1);
 }
 
 int main(int argc, char* argv[]) {
@@ -83,9 +158,16 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Usage: floodfill map_file_name\n");
         exit(1);
     }
+    srand(time(NULL));
     char* map_file = argv[1];
-    struct map *map = load_map(map_file);
+    Map *map = load_map(map_file);
+    if (!map) {
+        fprintf(stderr, "Failed to load map from %s.\n", map_file);
+        exit(2);
+    }
+
     print_map(map);
+    rec_flood_fill(map, 6, 4);
 
     exit(0);
 }
