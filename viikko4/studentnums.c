5d4c3a9f6bedfd943b4f7564d05051dce0706cdc
Laura Leppänen <laura.leppanen@iki.fi>
2011-09-28 23:54:01 +0300

    Random student number picker (ex 9).

---
+++ viikko4/studentnums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(void) {
+    FILE* studentnums = fopen("studentnumbers.txt", "r");
+    if (!studentnums) {
+        fprintf(stderr, "Could not open file.");
+        exit(1);
+    }
+
+    int line_len = 10;
+    fseek(studentnums, 0, SEEK_END);
+    unsigned long int len = ftell(studentnums);
+    int nums_len = len/line_len;
+
+    srand(time(NULL));
+    unsigned int student;
+    for (int i = 0; i < 3; i++) {
+        fseek(studentnums, (int) (rand() / (float) RAND_MAX * nums_len) * line_len,
+            SEEK_SET);
+        fscanf(studentnums, "%u\n", &student);
+        printf("%09u\n", student);
+    }
+
+    exit(0);
+}
